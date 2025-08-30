db9f32661841993d34efaa963229dcf3b4aed18e
fantaxmin <nicolas.torreslop@gmail.com>
2025-08-30 15:10:16 -0500

    style(DetailsCharacter): Ajuste de estilos de la especie y estado

--- src/components/DetailsCharacters.tsx
+++ src/components/DetailsCharacters.tsx
@@ -1,13 +1,13 @@
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router";
 import { CharacterContext } from "../context/CharactersProvider";
 import HeartIcon from "./icon/HeartIcon";
 
 const DetailsCharacters = ({ characterId } : { characterId: string }) => {
 
-    const { getCharacterById } = useContext(CharacterContext);
+    const { getCharacterById, handleFavoriteToggle } = useContext(CharacterContext);
     const navigate = useNavigate();
-    const character = useMemo(() => getCharacterById(Number(characterId)), [characterId]);
+    const character = getCharacterById(Number(characterId));
     if (!character) {
         navigate("/not-found");
         return null;
@@ -15,31 +15,30 @@ const DetailsCharacters = ({ characterId } : { characterId: string }) => {
 
     return (
             <section
-                className="flex flex-col pt-10 pl-20 max-sm:hidden"
+                className="flex flex-col w-full pt-10 px-20 max-sm:hidden"
             >
                 <div className="w-28 h-28 bg-gray-300 rounded-full relative mb-4">
                     {/* Character Image */}
-                    <button className="w-14 h-14 rounded-full bg-white absolute bottom-0 right-0">
+                    <button 
+                        className="w-14 h-14 rounded-full bg-white absolute bottom-0 right-0"
+                        onClick={() => handleFavoriteToggle(character.id)}
+                    >
                         <HeartIcon
-                            isFavorite={false}
+                            isFavorite={character.isFavorite}
                             className="justify-center items-center w-10 h-10 text-accent mx-auto my-auto cursor-pointer"
                         />
                     </button>
                 </div>
-                <h2 className="text-2xl font-bold">{character?.name}</h2>
-                <ul className="list-disc pl-5">
-                    <li>
-                        <span>Species:</span>
-                        <span>{character?.species}</span>
+                <h2 className="text-2xl font-bold mb-4">{character.name}</h2>
+                <ul className="list-disc">
+                    <li className="flex flex-col w-full py-5 border-t border-gray-200">
+                        <span className="font-bold">Specie</span>
+                        <span className="text-gray-500 text-md">{character.species}</span>
                     </li>
-                    <li>
-                        <span>Status:</span>
-                        <span>{character?.status}</span>
+                    <li className="flex flex-col w-full py-5 border-t border-gray-200">
+                        <span className="font-bold">Status</span>
+                        <span className="text-gray-500 text-md">{character.status}</span>
                     </li>
-                    {/* <li>
-                        <span>Occupation:</span>
-                        <span>{character?.occupation}</span>
-                    </li> */}
                 </ul>
 
             </section>
